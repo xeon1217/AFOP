49b3af8a30cfaa16ec57f39e57509b513b177d22
xeon1 <xeon1217@gmail.com>
2020-10-20 10:12:02 +0900

    Common 클래스를 UI 클래스로 바꿈

--- app/src/main/java/com/example/afop/util/UI.kt
+++ app/src/main/java/com/example/afop/util/UI.kt
@@ -1,26 +1,13 @@
 package com.example.afop.Utility
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.ProgressBar
-import androidx.cardview.widget.CardView
-import androidx.core.content.getSystemService
-import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 
-const val PARAM_EMAIL = "email"
-const val PARAM_NAME = "name"
-
-class Common {
+class UI {
     companion object {
-        fun enableBlock(context: Activity) {
+        fun showLoding(context: Activity) {
             context.progressDialog.visibility = View.VISIBLE
             context.progressBackground.visibility = View.VISIBLE
             context.window.setFlags(
@@ -33,7 +20,7 @@ class Common {
             )
         }
 
-        fun disableBlock(context: Activity) {
+        fun hideLoding(context: Activity) {
             context.progressDialog.visibility = View.GONE
             context.progressBackground.visibility = View.GONE
             context.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
