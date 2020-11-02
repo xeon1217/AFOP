ddd149bd96a76c4a46a84cacd07bd88cbf027054
xeon1 <xeon1217@gmail.com>
2020-11-02 11:04:41 +0900

    Result 모델 재설계

--- app/src/main/java/com/example/afop/data/model/Result.kt
+++ app/src/main/java/com/example/afop/data/model/Result.kt
@@ -4,7 +4,7 @@ package com.example.afop.data.model
  * 서버와 통신하는 등의 콜백에 사용 될 데이터 모델
  */
 data class Result<T> (
-        val data: T? = null,
-        val state: T? = null,
-        val error: Exception? = null
+        var data: T? = null,
+        var result: ResultModel? = null,
+        var error: Exception? = null
 )
---
+++ app/src/main/java/com/example/afop/data/model/ResultModel.kt
@@ -0,0 +1,5 @@
+package com.example.afop.data.model
+
+open class ResultModel {
+    val error: Int? = null
+}
