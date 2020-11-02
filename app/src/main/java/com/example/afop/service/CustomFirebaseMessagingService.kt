659ddb95e308c9b07db29f5298e7c27400006cc8
xeon1 <xeon1217@gmail.com>
2020-11-02 11:02:24 +0900

    로그 출력부 삭제

--- app/src/main/java/com/example/afop/service/CustomFirebaseMessagingService.kt
+++ app/src/main/java/com/example/afop/service/CustomFirebaseMessagingService.kt
@@ -17,7 +17,6 @@ import com.google.firebase.messaging.RemoteMessage
 class CustomFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token);
-        Log.d("FCM_TEST", token);
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
