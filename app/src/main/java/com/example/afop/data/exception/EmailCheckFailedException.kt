21368f2c904a9031f0a4c8131d7c28dc88454cc7
xeon1 <xeon1217@gmail.com>
2020-11-13 08:55:22 +0900

    예외처리 객체 삭제

--- app/src/main/java/com/example/afop/data/exception/EmailCheckFailedException.kt
+++
@@ -1,4 +0,0 @@
-package com.example.afop.data.exception
-
-//이메일 중복 확인에 실패한 경우
-class EmailCheckFailedException: Exception() {}
--- app/src/main/java/com/example/afop/data/exception/NickNameCheckFailedException.kt
+++
@@ -1,4 +0,0 @@
-package com.example.afop.data.exception
-
-//닉네임 중복 확인에 실패한 경우
-class NickNameCheckFailedException: Exception() {}
